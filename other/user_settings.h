0bf7ad7a4c1815ba56842df4242387bc5931c394
shana <jiatong@kth.se>
2018-07-21 18:33:47 +0200

    library and user_settings.h

---
+++ other/user_settings.h
@@ -0,0 +1,10 @@
+#ifndef USER_SETTINGS_H
+#define USER_SETTINGS_H
+
+#undef WOLFSSL_KEY_GEN // Sanity Guard against re-definition
+#define WOLFSSL_KEY_GEN
+
+// Add future settings here:
+
+
+#endif /* USER_SETTINGS_H */
